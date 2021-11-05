3eceb2712afff2cec3f6cac5feb19d36313db366
yacko abrams <yacko.abrams@capgemini.com>
2021-11-05 12:53:05 +0000

    fix for pytlint issues

--- calc.py
+++ calc.py
@@ -5,19 +5,19 @@
 
 def add(num_1, num_2):
     """
-    Function to add two numbers 
+    Function to add two numbers
     :param num_2: first number
     :param num_2: second number
-    :return: 
+    :return: sum of two numbers
     """
     return num_1 + num_2
 
 
 def subtract(num_1, num_2):
     """
-    Function to subtract two numbers 
+    Function to subtract two numbers
     :param num_2: first number
     :param num_2: second number
-    :return: 
+    :return: difference between two numbers
     """
     return num_1 - num_2
