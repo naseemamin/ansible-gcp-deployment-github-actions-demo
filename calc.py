f8e2a9339ab1d5d1c5c6dd0e0fdd63d4156d710f
yacko abrams <yacko.abrams@capgemini.com>
2021-11-05 12:49:25 +0000

    updated doc string and renamed variables

--- calc.py
+++ calc.py
@@ -3,9 +3,21 @@
 """
 
 
-def add(a, b):
-    return a + b
+def add(num_1, num_2):
+    """
+    Function to add two numbers 
+    :param num_2: first number
+    :param num_2: second number
+    :return: 
+    """
+    return num_1 + num_2
 
 
-def subtract(a, b):
-    return a - b
+def subtract(num_1, num_2):
+    """
+    Function to subtract two numbers 
+    :param num_2: first number
+    :param num_2: second number
+    :return: 
+    """
+    return num_1 - num_2
